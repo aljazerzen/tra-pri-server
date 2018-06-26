66c487cc1f00f1f8476de6b9799c2460304863ac
aljazerzen <aljaz.erzen@gmail.com>
2018-06-26 21:29:32 +0200

    feat: retain scroll position

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -8,40 +8,43 @@
 </template>
 
 <script>
-  import VueRouter from 'vue-router';
-  import routes from './routes';
-  import ErrorNotification from './components/error/ErrorNotification';
+import VueRouter from "vue-router";
+import routes from "./routes";
+import ErrorNotification from "./components/error/ErrorNotification";
 
-  export default {
-    name: 'app',
-    components: { ErrorNotification },
-    router: new VueRouter({ routes })
-  };
+export default {
+  name: "app",
+  components: { ErrorNotification },
+  router: new VueRouter({
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+      if (savedPosition) {
+        return savedPosition;
+      } else {
+        return { x: 0, y: 0 };
+      }
+    }
+  })
+};
 </script>
 
 <style lang="scss">
-  @import "~bulma/sass/utilities/initial-variables";
-  @import "~bulma/sass/utilities/functions";
+@import "~bulma/sass/utilities/initial-variables";
+@import "~bulma/sass/utilities/functions";
 
-  $widescreen: 1000px;
-  $fullhd: 1000px;
+$widescreen: 1000px;
+$fullhd: 1000px;
 
-  @import "~bulma/bulma";
+@import "~bulma/bulma";
 
-  #app {
-    font-family: 'Raleway', sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    min-height: 100%;
-  }
+#app {
+  font-family: "Raleway", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  min-height: 100%;
+}
 
-  .section.narrow {
-    /*max-width: 700px;*/
-    /*margin-right: auto;*/
-    /*margin-left: auto;*/
-  }
-
-  .section.columns {
-    max-width: 100%;
-  }
+.section.columns {
+  max-width: 100%;
+}
 </style>
