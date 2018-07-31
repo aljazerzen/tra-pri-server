4c44eb0d8fffa7305d7a2bfe44502adf4fc507bb
aljazerzen <aljaz.erzen@gmail.com>
2018-07-31 18:04:39 +0200

    feat: sidebar cleanup

--- webapp/src/components/dashboard/Sidebar.vue
+++ webapp/src/components/dashboard/Sidebar.vue
@@ -44,7 +44,6 @@
       <li>
         <router-link to="/paketi">Vsebinski paketi</router-link>
       </li>
-      <li><a>Statistika</a></li>
       <li><a v-on:click="logout()">Odjava</a></li>
     </ul>
   </aside>
